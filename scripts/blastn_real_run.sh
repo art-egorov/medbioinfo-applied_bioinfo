e208bba149138ebd132212cdb7d2e4e05698d4b5
art-egorov <artem.egorov@med.lu.se>
2022-06-15 11:18:52 +0200

    updating blastn scripts and tables for benchmarking

--- scripts/blastn_real_run.sh
+++ scripts/blastn_real_run.sh
@@ -3,11 +3,12 @@
 #SBATCH --partition=fast             # long, fast, etc.
 #SBATCH --ntasks=1                   # nb of *tasks* to be run in // (usually 1), this task can be multithreaded (see cpus-per-task)
 #SBATCH --nodes=1                    # nb of nodes to reserve for each task (usually 1)
-#SBATCH --cpus-per-task=3            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
+#SBATCH --cpus-per-task=2            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
 #SBATCH --mem=5GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
 #SBATCH --time=0-10:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
-#SBATCH -o ./slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names)
-#SBATCH -e ./slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
+#SBATCH --array=1-14   #SLURM_ARRAY_TASK_ID
+#SBATCH -o ./slurm_output/slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names)
+#SBATCH -e ./slurm_output/slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
 # /!\ Note that the ./outputs/ dir above needs to exist in the dir where script is submitted **prior** to submitting this script
 #SBATCH --job-name=blastn        # name of the task as displayed in squeue & sacc, also encouraged as srun optional parameter
 #SBATCH --mail-type END              # when to send an email notiification (END = when the whole sbatch array is finished)
@@ -17,21 +18,24 @@
 # Preparing work (cd to working dir, get hold of input data, convert/un-compress input data when needed etc.)
 workdir="/shared/projects/form_2022_19/artyom/sars2copath"
 blastn_database="/shared/projects/form_2022_19/artyom/sars2copath/data/blast_db/viral"
-query_files="/shared/projects/form_2022_19/artyom/sars2copath/data/subsets_of_reads_in_fasta"
+query_dir="/shared/projects/form_2022_19/artyom/sars2copath/data/merged_pairs"
+acc_numbers="/shared/projects/form_2022_19/artyom/sars2copath/analyses/aegorov_run_accessions.txt"
 
 echo START: `date`
 
 module load blast
+module load seqkit
 
 mkdir -p ${workdir}/data/blastn_output      # -p because it creates all required dir levels **and** doesn't throw an error if the dir exists :)
 cd ${workdir}
 
 
-for INPUT in $query_files/*.fa
-do
-echo "$INPUT" 
-srun blastn -num_threads ${SLURM_CPUS_PER_TASK} -evalue 10 -perc_identity 70 -db ${blastn_database} -outfmt 6  -query ${query_files}/$(basename $INPUT) -out ./data/blastn_output/$(basename $INPUT)_results.tsv 
-done
+accnum=$(sed -n "$SLURM_ARRAY_TASK_ID"p ${acc_numbers})
+input_file=${query_dir}/${accnum}.flash.extendedFrags.fastq.gz
+echo "$accnum"
+echo "$input_file"
+zcat ${input_file} | seqkit fq2fa | head
+srun --job-name=${accnum} seqkit fq2fa ${input_file} |  blastn -num_threads ${SLURM_CPUS_PER_TASK} -evalue 10e-5  -max_target_seqs 5 -perc_identity 70 -db ${blastn_database}   -outfmt 6 -out ./data/blastn_output/${accnum}_results.tsv 
  
 
 
